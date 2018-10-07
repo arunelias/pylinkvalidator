e10687e92fb6fbe5aa2a55190d2b545092021fea
Arun Elias <arun.testing3@gmail.com>
2018-10-07 17:37:02 +0000

    New option in report format - csv
    New option to select csv as report format

--- pylinkvalidator/models.py
+++ pylinkvalidator/models.py
@@ -81,6 +81,7 @@ PARSER_HTML5 = "html5lib"
 FORMAT_PLAIN = "plain"
 FORMAT_HTML = "html"
 FORMAT_JSON = "json"
+FORMAT_CSV = "csv"
 
 
 WHEN_ALWAYS = "always"
@@ -587,8 +588,8 @@ class Config(UTF8Class):
 
         output_group.add_option(
             "-f", "--format", dest="format", action="store",
-            default=FORMAT_PLAIN, choices=[FORMAT_PLAIN],
-            help="Format of the report: plain")
+            default=FORMAT_PLAIN, choices=[FORMAT_PLAIN, FORMAT_CSV],
+            help="Format of the report: plain(default) or csv")
         output_group.add_option(
             "-o", "--output", dest="output", action="store",
             default=None,
